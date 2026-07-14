a5372d35a0c1afadeac652fee867799032bb4af2
Muhammad Afzal <mafzal@c3r11s4.42warsaw.pl>
2026-07-14 16:53:27 +0200

    TwoSum Excecise Solved

---
+++ TwoSum/TwoSum.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{7, 2, 11, 15};
+
+        TwoSum twoSum = new TwoSum();
+
+        int target = 17;
+
+        int[] results = twoSum.twoSum(arr, target);
+
+        System.out.println(Arrays.toString(results));
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> res = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int remainder = target - nums[i];
+
+            if (res.containsKey(remainder)) {
+                return new int[]{res.get(remainder), i};
+            }
+
+            res.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
